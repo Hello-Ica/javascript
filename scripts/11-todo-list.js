ff1296e9b538841eba4ec5719ae48f7ad4621d0e
Ica <herrerajenica21@gmail.com>
2026-06-09 18:43:57 +0800

    loops and arrays 11a

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,10 +1,28 @@
-const ToDoList = []
-function AddToDo() {
-  const inputElement = document.querySelector('.js-name-input')
-  const name = inputElement.value
-  
-  ToDoList.push(name);
-  console.log(ToDoList);
+const ToDoList = ['make dinner', 'wash dishes']
+renderTodoList();
 
-  inputElement.value = '';
+function renderTodoList () {
+  let todoListHTML = '';
+
+  for (let i = 0; i < ToDoList.length; i++) {
+    const todo = ToDoList [i];
+    const html = `<p> ${todo} </p>`;
+    todoListHTML += html;
+  }
+
+  console.log(todoListHTML);
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
+
+  function AddToDo() {
+    const inputElement = document.querySelector('.js-name-input')
+    const name = inputElement.value
+    
+    ToDoList.push(name);
+    console.log(ToDoList);
+
+    inputElement.value = '';
+
+    renderTodoList();
+  }
+ 
